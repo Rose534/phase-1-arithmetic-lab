eb7e87fe69e564278baa2eb363392c39bafa33c5
rose534 <wambuirose534@gmail.com>
2022-11-24 14:04:14 +0300

    arthimetic

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let multiply;
+let num1 = 2;
+let num2 = 31;
+multiply = num1 * num2;
+console.log(multiply)
 
-// Write your code here
+
+let random; 
+random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+
+let mod;
+let num3 = 11
+let num4 = 7
+mod = num3 % num4
+console.log(mod)
+
+
+let max = Math.max(5, 20);
+console.log(max)
